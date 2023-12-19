74512dde3297690d5c92b65c7de3ff30e2fad382
Routine 66 <daniel.sinaga.ds@gmail.com>
2023-12-19 23:46:37 +0700

    update names on router

--- router/bookmarkRouter.js
+++ router/bookmarkRouter.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getBookmarkByUsername,
-  getBookmarkArticlesByUsername,
-  getBookmarkBatikByUsername,
-  getBookmarkCustomsByUsername,
+  getBookmarkByUid,
+  getBookmarkArticlesByUid,
+  getBookmarkBatikByUid,
+  getBookmarkCustomsByUid,
   postBookmark,
   deleteBookmark
 } = require("../controllers/bookmarkController");
 
 const { upload } = require('../app');
 
-router.get("/bookmarks/:username", getBookmarkByUsername);
-router.get("/bookmarks/articles/:username", getBookmarkArticlesByUsername);
-router.get("/bookmarks/batik/:username", getBookmarkBatikByUsername);
-router.get("/bookmarks/customs/:username", getBookmarkCustomsByUsername);
+router.get("/bookmarks/:uid", getBookmarkByUid);
+router.get("/bookmarks/articles/:uid", getBookmarkArticlesByUid);
+router.get("/bookmarks/batik/:uid", getBookmarkBatikByUid);
+router.get("/bookmarks/customs/:uid", getBookmarkCustomsByUid);
 router.post("/bookmarks/", postBookmark);
-router.delete("/bookmarks/:username/:contentId", deleteBookmark);
+router.delete("/bookmarks/:uid/:contentId", deleteBookmark);
 
 module.exports = router;
