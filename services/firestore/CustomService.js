56e17db03a157caf213ac576a193982919a23a6b
Routine 66 <daniel.sinaga.ds@gmail.com>
2023-12-21 17:44:40 +0700

    update custom service with fs module

--- services/firestore/CustomService.js
+++ services/firestore/CustomService.js
@@ -18,50 +18,49 @@ class CustomService {
     this.collectionRef = this.firestore.collection(this.collection);
   }
 
-  
-  async generateCustom(imageUrl){
+
+  async generateCustom(imageUrl) {
     try {
       const storage = new Storage({
         projectId: 'baskaryaapp',
         credentials: credentials,
       });
-      
+
       const bucket = storage.bucket('baskaryaapp.appspot.com');
-      // const url = 'https://asset-a.grid.id//crop/0x0:0x0/700x0/photo/2019/12/03/1846798683.jpg';
       const url = imageUrl;
 
       const bucketFolder = 'customImages';
-  
-  
+
+
       const response = await axios.get(url, { responseType: 'arraybuffer' });
       const imgBuffer = Buffer.from(response.data);
-  
+
       const image = await sharp(imgBuffer);
-  
+
       const overlayBuffer = fs.readFileSync('static/frame.png');
       const overlayImage = await sharp(overlayBuffer);
-  
+
       const baseImageMetadata = await image.metadata();
-  
+
       // Menyesuaikan ukuran base image agar memiliki dimensi yang sama
       const baseWidth = Math.min(baseImageMetadata.width, baseImageMetadata.height);
       const baseHeight = Math.min(baseImageMetadata.width, baseImageMetadata.height);
-  
+
       const baseImage = await image.resize({
         width: baseWidth,
         height: baseHeight,
         fit: 'contain',
       });
-  
+
       const resizedOverlayImage = await overlayImage.resize({
         width: baseWidth,
         height: baseHeight,
         fit: 'contain',
       });
-  
+
       // Composite base image and overlay image
       const twibbonImage = await baseImage.composite([{ input: await resizedOverlayImage.toBuffer(), gravity: 'center' }]);
-  
+
       // Create a new image with a transparent background
       const finalImage = sharp({
         create: {
@@ -71,36 +70,28 @@ class CustomService {
           background: { r: 0, g: 0, b: 0, alpha: 0 }
         }
       });
-  
+
       // Composite twibbonImage onto finalImage
       await finalImage.composite([{ input: await twibbonImage.toBuffer(), left: 0, top: 0 }]);
-  
+
       const timestamp = Date.now();
       const previewPathLocal = `custom-${timestamp}.png`;
       const previewPathUrl = `${bucketFolder}/${previewPathLocal}`;
-  
-      // Upload to Firebase Storage
+
+      // Save the final image
+      await finalImage.toFile(previewPathLocal);
+
+      // Upload to Firebase Storage or perform any other necessary operations
       const blob = bucket.file(previewPathUrl);
-      const uploadStream = blob.createWriteStream({
-        contentType: 'image/png',
-        resumable: false, // Set to true for larger files
-      });
-  
-      await finalImage.pipe(uploadStream); // Send image buffer directly to Firebase Storage
-  
-      // Handle stream events (optional)
-      return new Promise((resolve, reject) => {
-        uploadStream
-          .on('finish', () => {
-            const urlCustom = `https://firebasestorage.googleapis.com/v0/b/baskaryaapp.appspot.com/o/${encodeURIComponent(bucketFolder)}/${encodeURIComponent(previewPathLocal)}?alt=media`;
-            resolve(urlCustom);
-          })
-          .on('error', (error) => {
-            reject(new ClientError(error.message));
-          });
-      });
+      await blob.save(fs.createReadStream(previewPathLocal), { contentType: 'image/png' });
+
+      // Cleanup the local file
+      fs.unlinkSync(previewPathLocal);
+
+      const urlCustom = `https://firebasestorage.googleapis.com/v0/b/baskaryaapp.appspot.com/o/${bucketFolder}%2F${previewPathLocal}?alt=media`;
+      return urlCustom;
     } catch (error) {
-      throw new ClientError(error.message);
+      throw new ClientError(error.message)
     }
   }
 
